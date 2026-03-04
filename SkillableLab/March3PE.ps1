8cb2307d6d9a91b57858137f3d7af7f31b920bf4
colerquinn <cole.r.quinn@outlook.com>
2026-03-04 10:08:45 -0800

    4MARadditions

--- SkillableLab/March3PE.ps1
+++ SkillableLab/March3PE.ps1
@@ -1,10 +1,33 @@
-$OUName = "London"
+#Creates an Organizational Unit (OU) named "London" if it does not exist. 
+#If it does exist, write a message to the console stating that it already exists.
+$LondonOUName = "London"
 $DomainDN = "DC=Adatum,DC=com"
-$OUPath = "OU=$OUName,$DomainDN"
+$LondonOUPath = "OU=$OUName,$DomainDN" 
 
-if (Get-ADOrganizationalUnit -Identity $OUPath -ErrorAction SilentlyContinue) {
+if (Get-ADOrganizationalUnit -Identity $LondonOUPath -ErrorAction SilentlyContinue) {
     Write-Host "The London OU already exists."
 }
-else { (New-ADOrganizationalUnit -Name $OUName -Path $DomainDN)
+else { (New-ADOrganizationalUnit -Name $LondonOUName -Path $DomainDN)
         Write-Host "A New OU named London has been created"
+}
+
+#Inside the new "London" OU create a global security group named "London Users" 
+$GSGroupName = "London Users"
+$SalesOUName = "Sales"
+$SalesOUPath = "OU=$SalesOUName,$DomainDN"
+
+New-ADGroup -Name $GSGroupName -Path $LondonOUPath -GroupScope Global -GroupCategory Security
+       Write-Host "$GSGroupName Global Security Group has been created." 
+
+#Locate all users int the Sales OU that have an address in the city of London and move them into the London OU. 
+Get-ADUser -Filter "City -EQ 'London'" -SearchBase $SalesOUPath |
+        Move-ADObject -TargetPath $LondonOUPath
+        Write-Host "Users successfully moved to London OU"
+
+#Add the London OU users into the "London Users" security group. 
+$Users = Get-ADUser -SearchBase $LondonOUPath -Filter *
+
+ForEach ($user in $users) {
+        Add-ADGroupMember -Identity $GSGroupName -Members $user
+        Write-Host "Users sucessfully moved."
 }
